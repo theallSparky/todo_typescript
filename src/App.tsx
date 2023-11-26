b70fcbc212487297448a0178957ba10845968fa8
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-26 13:58:14 -0600

    Made 'InputField' component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <span className='heading'>Task-it</span>
-      Hello world!
+      <InputField />
     </div>
   );
 }
